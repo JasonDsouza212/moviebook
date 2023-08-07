66336b17344908ef753137185d3bfeb7719e1a7c
Your Name <you@example.com>
2023-08-07 20:54:41 +0530

    netlify deployment

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(express.json());
 
 // Enable CORS for all origins
-app.use(cors({
-  origin: "https://moviebookfrontend.vercel.app",
-  methods: ["POST", "GET", "DELETE"]
-}));
+app.use(cors());
 
 app.use('/api/playlists', playlistRoutes);
 app.use('/api/user', userRoutes);
