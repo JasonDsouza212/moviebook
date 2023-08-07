19daf469a0e13f73da26806252b901a1a13b81ac
Your Name <you@example.com>
2023-08-07 20:06:03 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,7 @@ app.use(express.json());
 // Enable CORS for requests from your frontend domain
 app.use(cors({
   origin: ["https://moviebookfrontend.vercel.app"],
-  methods: ["POST", "GET", "DELETE"],
-  credentials: true
+  methods: ["POST", "GET", "DELETE"]
 }));
 
 app.use('/api/playlists', playlistRoutes);
